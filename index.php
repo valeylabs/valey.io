c6fc16d616b0e9f694cc2b8e8f9177c28def7b2c
Gabriel Malaquias <gemalaquias@gmail.com>
2015-07-13 19:57:26 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Valey</title>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+    <style>
+        body,html{
+            padding: 0;
+            margin: 0;
+            min-height: 100%;
+            /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#f0f9ff+0,cbebff+47,a1dbff+100;Blue+3D+%2313 */
+            background: #f0f9ff; /* Old browsers */
+            background: -moz-linear-gradient(-45deg,  #f0f9ff 0%, #cbebff 47%, #a1dbff 100%); /* FF3.6+ */
+            background: -webkit-gradient(linear, left top, right bottom, color-stop(0%,#f0f9ff), color-stop(47%,#cbebff), color-stop(100%,#a1dbff)); /* Chrome,Safari4+ */
+            background: -webkit-linear-gradient(-45deg,  #f0f9ff 0%,#cbebff 47%,#a1dbff 100%); /* Chrome10+,Safari5.1+ */
+            background: -o-linear-gradient(-45deg,  #f0f9ff 0%,#cbebff 47%,#a1dbff 100%); /* Opera 11.10+ */
+            background: -ms-linear-gradient(-45deg,  #f0f9ff 0%,#cbebff 47%,#a1dbff 100%); /* IE10+ */
+            background: linear-gradient(135deg,  #f0f9ff 0%,#cbebff 47%,#a1dbff 100%); /* W3C */
+            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f0f9ff', endColorstr='#a1dbff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+            font-family: 'PT Sans Narrow', sans-serif;
+        }
+        .center{
+            width: 200px;
+            height: 200px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-left: -100px;
+            margin-top: -100px;
+        }
+
+        .center h2,p{text-align: center;}
+        .center a {text-decoration: none; color: blue}
+    </style>
+</head>
+<body>
+
+<div class="center">
+    <h2>Valey.io</h2>
+    <p><a href="http://github.com/valeylabs" target="_blank">http://github.com/valeylabs</a></p>
+</div>
+
+</body>
+</html>
